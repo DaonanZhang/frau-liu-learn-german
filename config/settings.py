e1ef29045aac2268d5383a6b0491b43bd1f58426
Daonan <dz@software-design.de>
2026-02-10 14:09:36 +0100

    some update

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,8 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
 environ.Env.read_env(BASE_DIR / ".env")
